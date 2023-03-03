e1dcd28ffc8197720ecc355dfd3c8b5d2203e9f8
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-03 15:25:37 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :blog do
-    resources :comments, only: [:create, :destroy]
-  end
+  resources :blogs
+  resources :comments, only: [:create, :destroy]
 end
